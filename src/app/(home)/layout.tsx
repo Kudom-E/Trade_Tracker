52d2a080e599cda710a9ce06f8becd9ecf91d6a3
Emmanuel <emmanuelkudomagyemang@gmail.com>
2023-06-26 15:37:40 +0000

    Removed Inter from layout

--- src/app/(home)/layout.tsx
+++ src/app/(home)/layout.tsx
@@ -1,9 +1,7 @@
 import "../globals.css";
 import React from "react";
-import { Inter } from "next/font/google";
+// import { Inter } from "next/font/google";
 import CombinedLayout from "./components/CombinedLayout";
-
-const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Create Next App",
